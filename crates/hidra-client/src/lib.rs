08064d218d087f0fbbccff5003250c47897c5cfc
James Wolff <james.wolff0@gmail.com>
2025-09-23 21:52:55 -0500

    Add update_state to client

--- crates/hidra-client/src/lib.rs
+++ crates/hidra-client/src/lib.rs
@@ -1,7 +1,7 @@
 #![deny(warnings)]
 
 use anyhow::{Result, bail};
-use hidra_ipc::{BrokerRequest, BrokerResponse, connect_client, read_json, write_json};
+use hidra_ipc::{BrokerRequest, BrokerResponse, PadState, connect_client, read_json, write_json};
 use hidra_protocol::DeviceKind;
 
 #[derive(Debug, Clone, Copy)]
@@ -28,8 +28,14 @@ pub async fn spawn(kind: DeviceKind) -> Result<GamepadHandle> {
     }
 }
 
-pub async fn set_rumble(_h: GamepadHandle, _low: u8, _high: u8) -> Result<()> {
-    Ok(())
+pub async fn update_state(h: GamepadHandle, s: PadState) -> Result<()> {
+    let mut pipe = connect_client().await?;
+    write_json(&mut pipe, &BrokerRequest::UpdateState { handle: h.0, state: s }).await?;
+    match read_json::<BrokerResponse, _>(&mut pipe).await? {
+        BrokerResponse::Ok => Ok(()),
+        BrokerResponse::Err { message } => bail!("broker error: {message}"),
+        other => bail!("unexpected response: {:?}", other),
+    }
 }
 
 pub async fn destroy(h: GamepadHandle) -> Result<()> {
