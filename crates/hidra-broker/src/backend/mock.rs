a003c734a554e9c3935b406ae84bc58789f268d8
James Wolff <james.wolff0@gmail.com>
2025-09-24 07:49:02 -0500

    feat: Per device routing in Mock backend

--- crates/hidra-broker/src/backend/mock.rs
+++ crates/hidra-broker/src/backend/mock.rs
@@ -1,15 +1,27 @@
 use super::Backend;
 use anyhow::Result;
 use dashmap::DashMap;
-use hidra_protocol::{DeviceKind, ioctl::PadState};
+use hidra_protocol::{
+    DeviceKind,
+    ioctl::PadState,
+    report::{DS4Report, DS5Report, X360Report},
+};
 use std::sync::{
     Arc,
     atomic::{AtomicU64, Ordering},
 };
+use tracing::debug;
 
 pub struct Mock {
     next: AtomicU64,
-    live: DashMap<u64, DeviceKind>,
+    live: DashMap<u64, Info>,
+}
+
+#[derive(Clone, Copy)]
+struct Info {
+    kind: DeviceKind,
+    #[allow(dead_code)] //TODO: remove once features are implemented
+    features: u32,
 }
 
 impl Mock {
@@ -20,16 +32,36 @@ impl Mock {
 
 #[async_trait::async_trait]
 impl Backend for Mock {
-    async fn create(&self, kind: DeviceKind, _features: u32) -> Result<u64> {
+    async fn create(&self, kind: DeviceKind, features: u32) -> Result<u64> {
         let h = self.next.fetch_add(1, Ordering::SeqCst);
-        self.live.insert(h, kind);
+        self.live.insert(h, Info { kind, features });
         Ok(h)
     }
+
     async fn destroy(&self, handle: u64) -> Result<()> {
         self.live.remove(&handle);
         Ok(())
     }
-    async fn update(&self, _h: u64, _s: PadState) -> Result<()> {
-        Ok(())
+
+    async fn update(&self, h: u64, s: PadState) -> Result<()> {
+        if let Some(info) = self.live.get(&h) {
+            match info.kind {
+                DeviceKind::X360 => {
+                    let rpt = X360Report::from(&s);
+                    debug!(handle = h, ?s, report = ?rpt, "mock update X360");
+                }
+                DeviceKind::DS4 => {
+                    let rpt = DS4Report::from(&s);
+                    debug!(handle = h, ?s, report = ?rpt, "mock update DS4");
+                }
+                DeviceKind::DS5 => {
+                    let rpt = DS5Report::from(&s);
+                    debug!(handle = h, ?s, report = ?rpt, "mock update DS5");
+                }
+            }
+            Ok(())
+        } else {
+            Err(anyhow::anyhow!("invalid handle: {}", h))
+        }
     }
 }
